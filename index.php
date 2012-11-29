d9e1f503c7786cabe8ca0f296e1cd18922ccbeb9
s0919202530 <s0919202530@gmail.com>
2012-11-29 13:20:58 +0800

    auto refresh 60 second

--- index.php
+++ index.php
@@ -88,6 +88,7 @@ if ((isset($_POST["insert"])) && ($_POST["insert"] == "add_shop"))
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="zh-tw" xml:lang="zh-tw">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta content='60' http-equiv='Refresh'/>
 		<title>訂午餐系統</title>
 		
 		<script type="text/javascript" src="JavaScript/ellipsis.js"></script>
